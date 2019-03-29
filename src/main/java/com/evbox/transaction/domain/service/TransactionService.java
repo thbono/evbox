3f40a232c950ce7ee81f01a16ebc48fe94bc1170
Tiago Bono <thbono@gmail.com>
2019-03-29 07:35:32 -0300

    Thread safe

--- src/main/java/com/evbox/transaction/domain/service/TransactionService.java
+++ src/main/java/com/evbox/transaction/domain/service/TransactionService.java
@@ -8,21 +8,19 @@ import com.evbox.transaction.domain.model.projection.TransactionTotalDTO;
 import com.google.common.base.Preconditions;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
 public class TransactionService {
 
     //TODO: add persistence layer with a time shard / time series database (e.g. InfluxDB, ElasticSearch)
-    private Map<String, Transaction> transactions = new HashMap<>();
+    private Map<String, Transaction> transactions = new ConcurrentHashMap<>();
 
-    @Transactional
     public Transaction create(final TransactionDTO transactionInfo) {
         Preconditions.checkNotNull(transactionInfo, "Transaction info cannot be null");
         final Transaction transaction = new Transaction(transactionInfo.getStationId());
@@ -30,7 +28,6 @@ public class TransactionService {
         return transaction;
     }
 
-    @Transactional
     public Transaction stop(final String id, final TransactionStopDTO stopInfo) {
         Preconditions.checkNotNull(stopInfo, "Transaction stop info cannot be null");
         final Transaction transaction = Optional.ofNullable(transactions.get(id)).orElseThrow(() -> new EmptyResultDataAccessException(1));
@@ -38,20 +35,17 @@ public class TransactionService {
         return transaction;
     }
 
-    @Transactional(readOnly = true)
     public TransactionTotalDTO getAllInLastMinute() {
         final TransactionTotalDTO total = new TransactionTotalDTO();
         transactions.values().stream().filter(Transaction::changedInLastMinute).forEach(total::add);
         return total;
     }
 
-    @Transactional(readOnly = true)
     public List<Transaction> getByStatusInLastMinute(final TransactionStatus status) {
         return transactions.values().stream().filter(Transaction::changedInLastMinute)
                 .filter(t -> t.isStatus(status)).collect(Collectors.toList());
     }
 
-    @Transactional
     public void deleteStopped() {
         final List<String> idsToRemote = transactions.entrySet().stream()
                 .filter(e -> e.getValue().isStopped()).map(Map.Entry::getKey).collect(Collectors.toList());
